8827007790d4e3335e710d4bde0e73fe0fec5a2b
RSA <ricardo.santos.araujo@cgd.pt>
2020-11-07 00:19:04 +0000

    project init

---
+++ src/main/java/com/cgd/tinkConnector/tinkConnector/TinkConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.cgd.tinkConnector.tinkConnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinkConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinkConnectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cgd/tinkConnector/tinkConnector/TinkConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgd.tinkConnector.tinkConnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinkConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
